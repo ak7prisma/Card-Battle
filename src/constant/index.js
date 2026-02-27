2baf4c92effefd6abc12f966bf41c8b0179d910b
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-02-27 20:58:28 +0700

    fix: import-card-icon

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,5 +1,3 @@
-import { TbPlayCard1, TbPlayCard10, TbPlayCard2, TbPlayCard3, TbPlayCard4, TbPlayCard5, TbPlayCard6, TbPlayCard7, TbPlayCard8, TbPlayCard9, TbPlayCardJ, TbPlayCardK, TbPlayCardQ } from "react-icons/tb";
-
 export const navLinks = [
   { name: "BATTLE", href: "#battle" },
   { name: "COLLECTION", href: "#collection" },
