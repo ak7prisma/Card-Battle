040e597766bc3ad80d81c060f5ad4c7b86f92686
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-05-17 22:12:30 +0700

    style: mobile responsive footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,11 +1,9 @@
-
-
 export default function Footer() {
-  
-  
   return (
-    <footer className="flex justify-center items-center p-5 border-t text-sm border-slate-300/20 text-slate-300/50 mt-20">
-      © by Ahmad Kurnia Prisma || ahmadkurniaprisma@gmail.com
+    <footer className="flex flex-col md:flex-row justify-center items-center gap-2 p-5 border-t text-xs md:text-sm border-slate-300/20 text-slate-300/50 mt-20 text-center">
+      <span>© by Ahmad Kurnia Prisma</span>
+      <span className="hidden md:inline text-slate-300/30">||</span>
+      <span>ahmadkurniaprisma@gmail.com</span>
     </footer>
   )
 }
